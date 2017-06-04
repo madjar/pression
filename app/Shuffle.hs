5f38cba98c802182b5d064e30a4cd03465d64c6e
Georges Dubus <georges.dubus@gmail.com>
2017-06-04 11:32:08 +0200

    Fix the shuffle feature

--- app/Shuffle.hs
+++ app/Shuffle.hs
@@ -6,12 +6,13 @@ import Pression.Library
 import Pression.Move
 import Fmt
 
+freeSize :: Integer
 freeSize = 100 * 1024 * 1024 * 1024
 
 main :: IO ()
 main =
   steamappsDirs >>= \case
-    [hot, cold] -> do fmtLn ("Hot: "#||hot||#"")
-                      fmtLn ("Cold: "#||cold||#"")
+    [hot, cold] -> do fmtLn ("Hot: "#|hot|#"")
+                      fmtLn ("Cold: "#|cold|#"")
                       shuffleAround hot cold freeSize
     _ -> fmtLn "Error, could not find two steam libraries"
--- src/Pression/Move.hs
+++ src/Pression/Move.hs
@@ -10,7 +10,7 @@ import Pression.Library
 import Control.Lens ((^?!), (^?), to)
 import System.FilePath ((</>), (<.>))
 import System.DiskSpace (getAvailSpace)
-import System.Directory (renameDirectory, renameFile, createDirectoryIfMissing)
+import System.Directory (createDirectoryIfMissing)
 import Data.Time.Clock.POSIX (getPOSIXTime)
 import Data.String.Conv (toS)
 import Control.Monad
@@ -19,9 +19,11 @@ import Fmt
 import System.IO.Unsafe (unsafePerformIO)
 import Data.LruCache.IO (newLruHandle, cached, LruHandle)
 import Data.List (sortOn)
+import Path (parseAbsDir, parseAbsFile)
+import Path.IO
 
 moveGame :: InstalledGame -> FilePath -> IO ()
-moveGame (InstalledGame from game) dest = do
+moveGame g@(InstalledGame from game) dest = do
   -- TODO check that steam is not running
   let manifestName = "appmanifest_" ++ show game <.> "acf"
   manifest <- parseSteamFile' (from </> manifestName)
@@ -32,11 +34,22 @@ moveGame (InstalledGame from game) dest = do
   when (availSpace < sizeOnDisk)
     (fail $ "No enough space left on "#|dest|#" ("#|availSpace|#" available, needs "#|sizeOnDisk|#")")
 
-  fmtLn ("Moving"#||game||#"to"#|dest|#".")
+  name <- gameName g
+  size <- getSizeOnDisk g
+  let sizeInGig = (fromInteger size) / 1024 / 1024 / 1024 :: Double
+  fmtLn ("Moving "#||name||#" to "#|dest|#" ("#|| sizeInGig ||#" GB).")
 
   createDirectoryIfMissing False (dest </> "common")
-  renameDirectory (from </> "common" </> installDir) (dest </> "common" </> installDir)
-  renameFile (from </> manifestName) (dest </> manifestName)
+
+  -- No recurive copy in base, so using the one from path0-io
+  fromDir <- parseAbsDir (from </> "common" </> installDir)
+  toDir <- parseAbsDir (dest </> "common" </> installDir)
+  fromManifest <- parseAbsFile (from </> manifestName)
+  toManifest <- parseAbsFile (dest </> manifestName)
+  copyDirRecur fromDir toDir
+  copyFile fromManifest toManifest
+  removeDirRecur fromDir
+  removeFile fromManifest
 
 user :: String
 user = "36588102"
@@ -51,8 +64,8 @@ parseSteamFile' path = cached steamCache path (parseSteamFile path)
 
 getLastPlayed :: GameId -> IO (Maybe Integer)
 getLastPlayed (GameId gid) = do
-  localconfig <- parseSteamFile (steamDir </> "userdata" </> user </> "config/localconfig.vdf")
-  return (read . toS <$> localconfig ^? key "UserLocalConfigStore" . key "Software" . key "Valve" . key "Steam" . key "Apps" . key (toS $ show gid) . key "LastPlayed" . _String)
+  localconfig <- parseSteamFile' (steamDir </> "userdata" </> user </> "config/localconfig.vdf")
+  return (read . toS <$> localconfig ^? key "UserLocalConfigStore" . key "Software" . key "Valve" . key "Steam" . key "apps" . key (toS $ show gid) . key "LastPlayed" . _String)
 
 playedRecently :: GameId -> IO Bool
 playedRecently g = do
@@ -60,8 +73,8 @@ playedRecently g = do
   case mlastPlayed of
     Just lastPlayed -> do
       now <- round <$> getPOSIXTime
-      let twoWeeksAgo = now - (60 * 60 * 24 * 7 * 2)
-      return (lastPlayed >= twoWeeksAgo)
+      let aMonthAgo = now - (60 * 60 * 24 * 30 * 1)
+      return (lastPlayed >= aMonthAgo)
     Nothing -> return False
 
 getSizeOnDisk :: InstalledGame -> IO Integer
@@ -69,22 +82,32 @@ getSizeOnDisk g = do
   manifest <- parseSteamFile' (libraryDir g </> manifestPath g)
   return (manifest ^?! key "AppState" . key "SizeOnDisk" . _String . to (read . toS))
 
+gameName g = do
+  manifest <- parseSteamFile' (libraryDir g </> manifestPath g)
+  return (manifest ^?! key "AppState" . key "name" . _String)
+
 shuffleAround :: FilePath -> FilePath -> Integer -> IO ()
 shuffleAround hot cold hotDesiredFreeSpace = do
   reheat <- filterM (playedRecently . gameId) =<< gamesInDir cold
   sizeOfReheat <- sum <$> traverse getSizeOnDisk reheat
-  let sizeToFree = sizeOfReheat + hotDesiredFreeSpace
+  availSpaceOnHot <- getAvailSpace hot
+  let sizeToFree = max 0 (sizeOfReheat + hotDesiredFreeSpace - availSpaceOnHot)
 
-  let addSize g = do size <- getSizeOnDisk g
-                     return (g, size)
-  candidatesForFreezing <- traverse addSize =<< (gamesInDir hot)
+  let addSizeAndLastPlayed g = do size <- getSizeOnDisk g
+                                  lastPlayed <- getLastPlayed (gameId g)
+                                  return ((g, size), lastPlayed)
+  gamesAndSizeAndLastPlayed <- traverse addSizeAndLastPlayed =<< (gamesInDir hot)
+  -- Don`t sort them by size, sort them by last played !
   let accum (!games, !size) (g, s) = (g:games, size+s)
-      freeze = fst . head . filter ( (>= sizeToFree). snd) . scanl accum ([], 0) . sortOn (negate . snd) $ candidatesForFreezing
+      candidatesForFreezing =  map fst . sortOn snd $ gamesAndSizeAndLastPlayed
+      freeze = reverse . fst . head . filter ( (>= sizeToFree). snd) . scanl accum ([], 0) $ candidatesForFreezing
 
+  let prettyGame g = (,) <$> gameName g <*> (getLastPlayed . gameId) g
   putStrLn "Reheat:"
-  print reheat
+  print =<< traverse prettyGame reheat
   putStrLn "Freeze:"
-  print freeze
+  print =<< traverse prettyGame freeze
+  print . sum =<< traverse getSizeOnDisk freeze
 
   traverse_ (\g -> moveGame g cold) freeze
   traverse_ (\g -> moveGame g hot) reheat
