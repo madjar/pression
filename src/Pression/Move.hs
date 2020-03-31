303f69a7bb8e722442bb32ed975ecc659c91b176
Georges Dubus <georges.dubus@gmail.com>
2020-03-31 15:27:34 +0200

    Allow to query which game to shuffle without shuffling

--- src/Pression/Move.hs
+++ src/Pression/Move.hs
@@ -1,26 +1,26 @@
-{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Pression.Move where
 
-import Pression.Types
-import Pression.Parser
-import Pression.Config
-import Pression.Library
-
-import Control.Lens ((^?!), (^?), to, failing)
-import System.FilePath ((</>), (<.>))
-import System.DiskSpace (getAvailSpace)
-import System.Directory (createDirectoryIfMissing, listDirectory)
-import Data.Time.Clock.POSIX (getPOSIXTime)
-import Data.String.Conv (toS)
+import Control.Lens ((^?), (^?!), failing, to)
 import Control.Monad
 import Data.Foldable
-import Fmt
-import System.IO.Unsafe (unsafePerformIO)
-import Data.LruCache.IO (newLruHandle, cached, LruHandle)
 import Data.List (sortOn)
+import Data.LruCache.IO (LruHandle, cached, newLruHandle)
+import Data.String.Conv (toS)
+import Data.Time.Clock.POSIX (getPOSIXTime)
+import Fmt
 import Path (parseAbsDir, parseAbsFile)
 import Path.IO
+import Pression.Config
+import Pression.Library
+import Pression.Parser
+import Pression.Types
+import System.Directory (createDirectoryIfMissing, listDirectory)
+import System.DiskSpace (getAvailSpace)
+import System.FilePath ((<.>), (</>))
+import System.IO.Unsafe (unsafePerformIO)
 
 moveGame :: InstalledGame -> FilePath -> IO ()
 moveGame g@(InstalledGame from game) dest = do
@@ -29,18 +29,15 @@ moveGame g@(InstalledGame from game) dest = do
   manifest <- parseSteamFile' (from </> manifestName)
   let installDir = manifest ^?! key "AppState" . key "installdir" . _String . to toS
       sizeOnDisk = manifest ^?! key "AppState" . key "SizeOnDisk" . _String . to (read . toS)
-
   availSpace <- getAvailSpace dest
-  when (availSpace < sizeOnDisk)
-    (fail $ "No enough space left on "+|dest|+" ("+|availSpace|+" available, needs "+|sizeOnDisk|+")")
-
+  when
+    (availSpace < sizeOnDisk)
+    (fail $ "No enough space left on " +| dest |+ " (" +| availSpace |+ " available, needs " +| sizeOnDisk |+ ")")
   name <- gameName g
   size <- getSizeOnDisk g
   let sizeInGig = (fromInteger size) / 1024 / 1024 / 1024 :: Double
-  fmtLn ("Moving "+||name||+" to "+|dest|+" ("+|| sizeInGig ||+" GB).")
-
+  fmtLn ("Moving " +|| name ||+ " to " +| dest |+ " (" +|| sizeInGig ||+ " GB).")
   createDirectoryIfMissing False (dest </> "common")
-
   -- No recurive copy in base, so using the one from path0-io
   fromDir <- parseAbsDir (from </> "common" </> installDir)
   toDir <- parseAbsDir (dest </> "common" </> installDir)
@@ -50,7 +47,6 @@ moveGame g@(InstalledGame from game) dest = do
   copyFile fromManifest toManifest
   removeDirRecur fromDir
   removeFile fromManifest
-
 
 -- TODO Make that cache nicer
 {-# NOINLINE steamCache #-}
@@ -71,14 +67,15 @@ getLastPlayed (GameId gid) = do
         parseSteamFile'
           (steamDir </> "userdata" </> user </> "config/localconfig.vdf")
       return
-        (read . toS <$> localconfig ^? key "UserLocalConfigStore" .
-         key "Software" .
-         key "Valve" .
-         key "Steam" .
-         failing (key "apps") (key "Apps") . -- This seems to be different on windows and mac
-         key (toS $ show gid) .
-         key "LastPlayed" .
-         _String)
+        ( read . toS <$> localconfig ^? key "UserLocalConfigStore"
+            . key "Software"
+            . key "Valve"
+            . key "Steam"
+            . failing (key "apps") (key "Apps")
+            . key (toS $ show gid) -- This seems to be different on windows and mac
+            . key "LastPlayed"
+            . _String
+        )
 
 playedRecently :: GameId -> IO Bool
 playedRecently g = do
@@ -99,28 +96,31 @@ gameName g = do
   manifest <- parseSteamFile' (libraryDir g </> manifestPath g)
   return (manifest ^?! key "AppState" . key "name" . _String)
 
-shuffleAround :: FilePath -> FilePath -> Integer -> IO ()
-shuffleAround hot cold hotDesiredFreeSpace = do
+gamesToShuffleAround :: FilePath -> FilePath -> Integer -> IO ([InstalledGame], [InstalledGame])
+gamesToShuffleAround hot cold hotDesiredFreeSpace = do
   reheat <- filterM (playedRecently . gameId) =<< gamesInDir cold
   sizeOfReheat <- sum <$> traverse getSizeOnDisk reheat
   availSpaceOnHot <- getAvailSpace hot
   let sizeToFree = max 0 (sizeOfReheat + hotDesiredFreeSpace - availSpaceOnHot)
-
-  let addSizeAndLastPlayed g = do size <- getSizeOnDisk g
-                                  lastPlayed <- getLastPlayed (gameId g)
-                                  return ((g, size), lastPlayed)
-  gamesAndSizeAndLastPlayed <- traverse addSizeAndLastPlayed =<< (gamesInDir hot)
+  let addSizeAndLastPlayed g = do
+        size <- getSizeOnDisk g
+        lastPlayed <- getLastPlayed (gameId g)
+        return ((g, size), lastPlayed)
+  gamesAndSizeAndLastPlayed <- traverse addSizeAndLastPlayed =<< gamesInDir hot
   -- Don`t sort them by size, sort them by last played !
-  let accum (!games, !size) (g, s) = (g:games, size+s)
-      candidatesForFreezing =  map fst . sortOn snd $ gamesAndSizeAndLastPlayed
-      freeze = reverse . fst . head . filter ( (>= sizeToFree). snd) . scanl accum ([], 0) $ candidatesForFreezing
+  let accum (!games, !size) (g, s) = (g : games, size + s)
+      candidatesForFreezing = map fst . sortOn snd $ gamesAndSizeAndLastPlayed
+      freeze = reverse . fst . head . filter ((>= sizeToFree) . snd) . scanl accum ([], 0) $ candidatesForFreezing
+  return (reheat, freeze)
 
+shuffleAround :: FilePath -> FilePath -> Integer -> IO ()
+shuffleAround hot cold hotDesiredFreeSpace = do
+  (reheat, freeze) <- gamesToShuffleAround hot cold hotDesiredFreeSpace
   let prettyGame g = (,) <$> gameName g <*> (getLastPlayed . gameId) g
   putStrLn "Reheat:"
   print =<< traverse prettyGame reheat
   putStrLn "Freeze:"
   print =<< traverse prettyGame freeze
   print . sum =<< traverse getSizeOnDisk freeze
-
   traverse_ (\g -> moveGame g cold) freeze
   traverse_ (\g -> moveGame g hot) reheat
