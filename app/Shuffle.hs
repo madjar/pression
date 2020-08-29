bda5c694925713076ba188b3f1660272d447ee30
Georges Dubus <georges.dubus@gmail.com>
2020-08-29 11:20:12 +0200

    Add --dry-run for shuffle

--- app/Shuffle.hs
+++ app/Shuffle.hs
@@ -6,6 +6,8 @@ module Main where
 import Fmt
 import Pression.Library
 import Pression.Move
+import System.Environment (getArgs)
+import Control.Monad (when)
 
 freeSize :: Integer
 freeSize = 100 * 1024 * 1024 * 1024
@@ -18,5 +20,7 @@ main =
       fmtLn ("Cold: " +| coldPath |+ "")
       shuffleResult <- gamesToShuffleAround hotPath coldPath freeSize
       printShuffleResult shuffleResult
-      shuffleAround shuffleResult
+      args <- getArgs
+      when ("--dry-run" `notElem` args) $ do
+        shuffleAround shuffleResult
     _ -> fmtLn "Error, could not find two steam libraries"
